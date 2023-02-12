8f93ee1c05e5f52f2d3a53bec7aba0ec17113088
Özlem Salman <ozlem1salman@gmail.com>
2023-02-12 13:27:44 +0300

    Added ozlem.py

---
+++ ozlem.py
@@ -0,0 +1,13 @@
+from match import compare_data_cross_species
+
+
+"""compare_data_cross_species( \
+    "./AMELBlastin/Homo sapiens (Human)_Ameloblastin12_length.csv" \
+    , "./target" \
+    , "./twelveAAHAmat.csv")"""
+    
+
+compare_data_cross_species( \
+    "./Enamelin/Homo sapiens (Human)_Enamelin12_length.csv" \
+    , "./target" \
+    , "./twelveAAHAmat.csv")
